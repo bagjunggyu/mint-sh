bf30fc29f0a88ca0163473e41fbcce6950e290f0
박정규 <bagjunggyu@gmail.com>
2025-11-30 13:01:18 +0900

    Create mint.sh
    리눅스 민트 22.x 설치 후 설정 자동 스크립트

---
+++ mint.sh
@@ -0,0 +1,56 @@
+### Linux Mint 22.x 설치 후 개인 설정 자동화 작업 ###
+### 맥북으로 설하는 경우에는 bcmwl-kernel 소스 설치 및 fusuma 설치
+### mint-sh 폴더에서 작업
+### 선호하는 clementine, moc, gparted, boot-repair, ranger, tilda, mpv를 설치하고
+### 잘 사용하지 않는 rhythmbox, celluloid,  thunderbird를 제거합니다.
+### vsftpd, ssh를 설치하고 미리 작업해 둔 내용으로 설정합니다.
+### 한글 입력기 kime를 설치하고 미리 설정한 값으로 지정합니다.
+### https://blog.naver.com/bagjunggyu/223996937015 , https://blog.naver.com/bagjunggyu/223797346563 참고하세요.
+
+#!/bin/bash
+apt update
+apt remove rhythmbox celluloid thunderbird -y
+apt install clementine moc gparted boot-repair ssh vsftpd ranger tilda mpv -y
+apt dist-upgrade -y
+
+### bcmwl-kernel-source for intel macbook
+#wget http://security.ubuntu.com/ubuntu/pool/restricted/b/bcmwl/bcmwl-kernel-source_6.30.223.271+bdcom-0ubuntu10~22.04.1_amd64.deb
+#apt deb bcmwl-kernel-source_6.30.223.271+bdcom-0ubuntu10~22.04.1_amd64.deb
+#apt install -f -y
+
+### fusuma
+#sudo gpasswd -a $USER input
+#apt install libinput-tools ruby xdotool -y
+#sudo gem install fusuma
+#mkdir -p ~/.config/fusuma
+#mv config.yml ~/.config/fusuma
+
+### kime
+wget https://github.com/Riey/kime/releases/download/v3.1.1/kime_ubuntu-22.04_v3.1.1_amd64.deb
+apt deb kime_ubuntu-22.04_v3.1.1_amd64.deb
+sudo mkdir /etc/xdg/kime
+sudo mv config.yaml /etc/xdg/kime
+im-config -n kime
+
+### ssh & ftp & ranger setting
+sudo mv sshd_config /etc/ssh
+sudo mv vsftpd.conf /etc
+sudo mv rc.conf /etc/ranger/config
+sudo mv rifle.conf /lib/python3/dist-packages/ranger/config
+sudo service ssh start
+sudo service vsftpd start
+
+### moc setting
+mkdir -p ~/.moc
+touch ~/.moc/config
+sh -c 'echo "Theme=/usr/share/moc/themes/transparent-background" >> ~/.moc/config'
+sudo chmod 755 ~/.moc/config
+
+### Affecting Linux Mint 22.2 Zara Cinnamon's Korean translation
+sudo mv *.mo /usr/share/locale/ko/LC_MESSAGES/
+
+### Delete files & reboot
+rm *.*
+apt autoremove --purge -y
+apt autoclean clean -y
+sudo reboot
